10061baf0486d788494756ac3ba5fb52df5091da
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-21 18:36:04 +0530

    Update main.tf
    deploy-to-production

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired region
+}
+
 #s3 bucket
 resource "aws_s3_bucket" "s3" {
   bucket = var.bucket
   object_lock_enabled = true
-  region = "us-west-2"
+  
 }
 
 #bucket ownership
