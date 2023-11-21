74a44cb0662a41d57ad22878ce9233f179d0ff66
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-21 19:55:30 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"  # Replace with your desired region  
 }
 
-#s3 bucket
+#s3 bucket 
 resource "aws_s3_bucket" "s3" {
   bucket = var.bucket
   object_lock_enabled = true
