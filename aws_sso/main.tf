06cecabfe88b87b9cd598bc5df51954b2880694f
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-21 19:52:45 +0530

    Update main.tf

--- aws_sso/main.tf
+++ aws_sso/main.tf
@@ -1,5 +1,5 @@
 # Create an AWS SSO permission set
-resource "aws_ssoadmin_permission_set" "permission_set" {
+resource "aws_ssoadmin_permission_set" "ex_permission_set" {
   name        = var.permission_set_name
   description = var.permission_set_desc
   instance_arn = var.sso_instance_arn
@@ -10,7 +10,7 @@ resource "aws_ssoadmin_account_assignment" "account_assignment" {
   instance_arn        = var.sso_instance_arn
   target_type         = "AWS_ACCOUNT"
   target_id           = var.account_id
-  permission_set_arn  = aws_ssoadmin_permission_set.permission_set.arn
+  permission_set_arn  = aws_ssoadmin_permission_set.ex_permission_set.arn
   principal_type      = "GROUP"
   principal_id = var.group_id
 }
