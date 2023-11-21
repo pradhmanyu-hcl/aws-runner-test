34343fcc411bf878254aa9e7676dbb1d637ed192
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-21 19:37:07 +0530

    Update main.tf

--- aws_sso/main.tf
+++ aws_sso/main.tf
@@ -1,5 +1,5 @@
 # Create an AWS SSO permission set
-resource "aws_ssoadmin_permission_set" "example_permission_set" {
+resource "aws_ssoadmin_permission_set" "permission_set" {
   name        = var.permission_set_name
   description = var.permission_set_desc
   instance_arn = var.sso_instance_arn
@@ -13,4 +13,10 @@ resource "aws_ssoadmin_account_assignment" "example_account_assignment" {
   permission_set_arn  = aws_ssoadmin_permission_set.example_permission_set.arn
   principal_type      = "GROUP"
   principal_id = var.group_id
+}
+
+#Attaching Policy to permissionSets
+resource "aws_ssoadmin_managed_policy_attachment" "policy_attachment" {
+  permission_set_name = aws_ssoadmin_permission_set.example_permission_set.name
+  managed_policy_arn  = var.managed_policy_arn
 }
