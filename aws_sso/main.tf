6584db5970559fc87916dd4841d43f51e32fdce3
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-21 20:31:11 +0530

    Update main.tf

--- aws_sso/main.tf
+++ aws_sso/main.tf
@@ -1,4 +1,4 @@
-# Create an AWS SSO permission set 
+# Create an AWS SSO permission set  
 resource "aws_ssoadmin_permission_set" "example_permission_set" {
   name        = var.permission_set_name
   description = var.permission_set_desc
