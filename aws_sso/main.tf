ac6b42dc018079bf08c8a2f884075bb7955c3cf6
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-21 19:40:10 +0530

    Update main.tf
    deploy-to-management

--- aws_sso/main.tf
+++ aws_sso/main.tf
@@ -10,13 +10,13 @@ resource "aws_ssoadmin_account_assignment" "example_account_assignment" {
   instance_arn        = var.sso_instance_arn
   target_type         = "AWS_ACCOUNT"
   target_id           = var.account_id
-  permission_set_arn  = aws_ssoadmin_permission_set.example_permission_set.arn
+  permission_set_arn  = aws_ssoadmin_permission_set.permission_set.arn
   principal_type      = "GROUP"
   principal_id = var.group_id
 }
 
 #Attaching Policy to permissionSets
 resource "aws_ssoadmin_managed_policy_attachment" "policy_attachment" {
-  permission_set_name = aws_ssoadmin_permission_set.example_permission_set.name
+  permission_set_name = aws_ssoadmin_permission_set.permission_set.name
   managed_policy_arn  = var.managed_policy_arn
 }
