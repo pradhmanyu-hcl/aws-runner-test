ed7f1c668cc8d468e1c17a31a9c299d655ed3e2b
jarialp <151749393+jarialp@users.noreply.github.com>
2023-11-27 20:07:46 +0530

    Delete application_vpc directory

--- application_vpc/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform{
-    backend "s3" {
-        bucket = "testbucket11152023"
-        key = "vpc.tfstate"
-        region = "us-west-2"
-        profile = "default"
-    }
-
-}
--- application_vpc/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "Region name in which the vpc need to be created"
-  type = string
-  default = "us-west-2"
-}
