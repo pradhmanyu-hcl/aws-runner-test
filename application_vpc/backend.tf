93c5f57dfbdc80da59dd6aec4c11f0f5e285efa6
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-27 22:03:07 +0530

    deploy

--- application_vpc/backend.tf
+++ application_vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "testbucket11152023"
+        bucket = "tfstste-backend-tmo"
         key = "appvpc.tfstate"
         region = "us-west-2"
         profile = "default"
