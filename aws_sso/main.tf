d46778615f6b88b5d26fe23dd945333f2171ca20
pradhmanyu-hcl <151548145+pradhmanyu-hcl@users.noreply.github.com>
2023-11-28 19:28:40 +0530

    build

--- aws_sso/main.tf
+++ aws_sso/main.tf
@@ -6,7 +6,7 @@ resource "aws_ssoadmin_permission_set" "example_permission_set" {
   instance_arn = var.sso_instance_arn
 }
 
-# Attach Group to Permission Set 
+# Attach Group to Permission Set
 resource "aws_ssoadmin_instance_access_control_attribute_configuration" "sso_group_permission_set_attachment" {
   instance_arn   = var.sso_instance_arn
   access_control_attribute_name = "aws:PrincipalTag/GroupName"
