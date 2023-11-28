615054e1155724fa1b438f50f1916e3a9fbc8353
jarialp <151749393+jarialp@users.noreply.github.com>
2023-11-28 20:02:15 +0530

    deploy-to-production

--- aws_sso/main.tf
+++ aws_sso/main.tf
@@ -6,36 +6,35 @@ resource "aws_ssoadmin_permission_set" "example_permission_set" {
   instance_arn = var.sso_instance_arn
 }
 
-# Attach Group to Permission Set
-resource "aws_ssoadmin_instance_access_control_attributes" "sso_group_permission_set_attachment" {
-  instance_arn   = var.sso_instance_arn
-  #access_control_attribute_name = "aws:PrincipalTag/GroupName"
-  attribute {
-  key            = var.group_name
-  value    {
-    source      = [aws_ssoadmin_permission_set.example_permission_set.name]
-    }
-  }
-}
-
-# Attach AWS SSO Managed Policy to Permission Set
-resource "aws_ssoadmin_managed_policy_attachment" "sso_managed_policy_attachment" {
-  instance_arn      = var.sso_instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.example_permission_set.arn
-  managed_policy_arn = var.managed_policy_arn  # Example policy, change as needed
-}
-
-## Update Group access level and Permission Set
-#resource "aws_ssoadmin_instance_access_control_attributes" "sso_group_permission_set_update" {
+## Attach Group to Permission Set
+#resource "aws_ssoadmin_instance_access_control_attributes" "sso_group_permission_set_attachment" {
 #  instance_arn   = var.sso_instance_arn
-## access_control_attribute_name = "aws:PrincipalTag/GroupName"
 #  attribute {
 #  key            = var.group_name
-#  value  {
-#      source    = [aws_ssoadmin_permission_set.example_permission_set.name]
+#  value    {
+#    source      = [aws_ssoadmin_permission_set.example_permission_set.name]
 #    }
 #  }
 #}
+
+
+## Attach AWS SSO Managed Policy to Permission Set
+#resource "aws_ssoadmin_managed_policy_attachment" "sso_managed_policy_attachment" {
+#  instance_arn      = var.sso_instance_arn
+#  permission_set_arn = aws_ssoadmin_permission_set.example_permission_set.arn
+#  managed_policy_arn = var.managed_policy_arn  # Example policy, change as needed
+#}
+
+# Update Group access level and Permission Set
+resource "aws_ssoadmin_instance_access_control_attributes" "sso_group_permission_set_update" {
+  instance_arn   = var.sso_instance_arn
+  attribute {
+  key            = var.group_name
+  value  {
+      source    = [aws_ssoadmin_permission_set.example_permission_set.name]
+    }
+  }
+}
  
 ## Revoke Access by Detaching Group from Permission Set
 #resource "aws_ssoadmin_instance_access_control_attributes" "sso_group_permission_set_detachment" {
@@ -49,12 +48,12 @@ resource "aws_ssoadmin_managed_policy_attachment" "sso_managed_policy_attachment
 #   }
 #}
 
-# Create an AWS SSO account assignment for a group 
-resource "aws_ssoadmin_account_assignment" "example_account_assignment" {
-  instance_arn        = var.sso_instance_arn
-  target_type         = "AWS_ACCOUNT"
-  target_id           = var.account_id
-  permission_set_arn  = aws_ssoadmin_permission_set.example_permission_set.arn
-  principal_type      = "GROUP"
-  principal_id = var.group_name
-}
+## Create an AWS SSO account assignment for a group 
+#resource "aws_ssoadmin_account_assignment" "example_account_assignment" {
+#  instance_arn        = var.sso_instance_arn
+#  target_type         = "AWS_ACCOUNT"
+#  target_id           = var.account_id
+#  permission_set_arn  = aws_ssoadmin_permission_set.example_permission_set.arn
+#  principal_type      = "GROUP"
+#  principal_id = var.group_name
+#}
